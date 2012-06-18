e40ca11106fac0a7586da5aacb1fe8f84c59fb58
SenikTony <ansenik@yandex.ru>
2012-06-18 16:26:03 +0400

    SenikTony tic_tac_toe

--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -5,26 +5,39 @@ module TicTacToe
     attr_reader :players, :turns, :winner, :turns_left, :current_player
 
     def initialize
-      @players = []
-      @turns   = [[], [], []]
-      @winner  = ''
-      @turns_left = 0
-      @current_player = ''
+      @players = ['X', '0']
+      @turns   = [Array.new(3, ' '), Array.new(3, ' '), Array.new(3, ' ')]
+      @winner  = nil
+      @turns_left = 9
+      @current_player = 0
     end
 
     def turn(x, y)
+      turns[x][y] = players[current_player]
     end
 
     def can_turn?(x, y)
+      !players.include?(turns[x][y])
     end
 
     def has_winner?
+      players.include?(winner)
     end
 
     def finished?
+      if (has_winner? || !has_turns?)
+        true
+      else
+        false
+      end
     end
 
     def has_turns?
+      if turns_left > 0
+        true
+      else
+        false
+      end
     end
 
     def print_turns
